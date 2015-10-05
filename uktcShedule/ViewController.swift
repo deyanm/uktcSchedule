f8a9278a4c88f59fe2452ebd548f49c3a9e65487
–î–µ–Ω–∏ <deian_deni@abv.bg>
2015-10-05 07:41:00 +0300

    Initial Commit

---
+++ uktcShedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uktcShedule
+//
+//  Created by Deyan Marinov on 10/5/15.
+//  Copyright © 2015 Deyan Marinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
